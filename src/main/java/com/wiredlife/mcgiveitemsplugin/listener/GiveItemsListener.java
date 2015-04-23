9f18a1b89564e067e783dc4807af8a816a8828b1
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-23 22:20:06 +0200

    Updated comments

--- src/main/java/com/wiredlife/mcgiveitemsplugin/listener/GiveItemsListener.java
+++ src/main/java/com/wiredlife/mcgiveitemsplugin/listener/GiveItemsListener.java
@@ -9,15 +9,22 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 public class GiveItemsListener implements Listener {
-	
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent evt) {
-        Player player = evt.getPlayer(); // The player who joined
-        System.out.println("User joined: " + player.getName());
-        PlayerInventory inventory = player.getInventory(); // The player's inventory
-        ItemStack itemstack = new ItemStack(Material.DIRT, 2); // A small stack of dirt
-     
-        inventory.addItem(itemstack); // Adds a stack of dirt to the player's inventory
-    }
-	
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		// The player who joined
+		Player player = event.getPlayer();
+
+		System.out.println("User joined: " + player.getName());
+
+		// The player's inventory
+		PlayerInventory inventory = player.getInventory();
+
+		// A small stack of dirt
+		ItemStack itemStack = new ItemStack(Material.DIRT, 2);
+
+		// Adds a stack of dirt to the player's inventory
+		inventory.addItem(itemStack);
+	}
+
 }

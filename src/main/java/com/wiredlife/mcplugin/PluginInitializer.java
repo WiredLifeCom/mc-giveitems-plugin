1f79cb4c6b5d0a2bc19143f4628b7f510bb126ed
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-27 20:48:47 +0200

    Changed structure for initializing plugin

---
+++ src/main/java/com/wiredlife/mcplugin/PluginInitializer.java
@@ -0,0 +1,21 @@
+package com.wiredlife.mcplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.wiredlife.mcplugin.listener.GiveItemsListener;
+
+public class PluginInitializer extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Insert logic to be performed when the plugin is enabled
+
+		getServer().getPluginManager().registerEvents(new GiveItemsListener(), this);
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
+
+}

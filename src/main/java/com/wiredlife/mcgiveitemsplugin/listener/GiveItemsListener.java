4eb5e9569be6a2c530b8b9bf006c1f40d9f54538
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-27 11:14:25 +0200

    Added map for resource to material mapping

--- src/main/java/com/wiredlife/mcgiveitemsplugin/listener/GiveItemsListener.java
+++ src/main/java/com/wiredlife/mcgiveitemsplugin/listener/GiveItemsListener.java
@@ -1,6 +1,11 @@
 package com.wiredlife.mcgiveitemsplugin.listener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,12 +17,16 @@ public class GiveItemsListener implements Listener {
 	
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent evt) {
-        Player player = evt.getPlayer(); // The player who joined
+        Map<String, Material> map = new HashMap<String, Material>();
+        map.put("Dirt", Material.DIRT);
+        map.put("Stone", Material.STONE);
+    	
+    	Player player = evt.getPlayer(); // The player who joined
         System.out.println("User joined: " + player.getName());
         PlayerInventory inventory = player.getInventory(); // The player's inventory
-        ItemStack itemstack = new ItemStack(Material.DIRT, 2); // A small stack of dirt
+        ItemStack itemStack = new ItemStack(map.get("Dirt"), 2); // A small stack of dirt
      
-        inventory.addItem(itemstack); // Adds a stack of dirt to the player's inventory
+        inventory.addItem(itemStack); // Adds a stack of dirt to the player's inventory
     }
 	
 }

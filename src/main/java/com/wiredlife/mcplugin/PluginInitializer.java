b3e56c618628844ef43626a24c3a24d886ed9826
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-05-21 20:12:03 +0200

    Renamed class

--- src/main/java/com/wiredlife/mcplugin/PluginInitializer.java
+++ src/main/java/com/wiredlife/mcplugin/PluginInitializer.java
@@ -2,39 +2,39 @@ package com.wiredlife.mcplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.wiredlife.mcplugin.concurrent.UpdateResourcesRunnable;
+import com.wiredlife.mcplugin.concurrent.UpdateMaterialsRunnable;
 import com.wiredlife.mcplugin.config.Config;
 import com.wiredlife.mcplugin.listener.OnHitSendPokeListener;
 import com.wiredlife.mcplugin.listener.OnJoinUpdateResourcesListener;
 
 public class PluginInitializer extends JavaPlugin {
 
-	Thread updateResourcesThread;
+	Thread updateMaterialsThread;
 
-	UpdateResourcesRunnable updateResourcesRunnable;
+	UpdateMaterialsRunnable updateMaterialsRunnable;
 
 	@Override
 	public void onEnable() {
 		// TODO Insert logic to be performed when the plugin is enabled
-		
+
 		Config.getValues().put("database", "../data/database.sqlite");
 
 		getServer().getPluginManager().registerEvents(new OnJoinUpdateResourcesListener(), this);
 		getServer().getPluginManager().registerEvents(new OnHitSendPokeListener(), this);
 
-		this.updateResourcesRunnable = new UpdateResourcesRunnable();
+		this.updateMaterialsRunnable = new UpdateMaterialsRunnable();
 
-		this.updateResourcesThread = new Thread(this.updateResourcesRunnable);
-		this.updateResourcesThread.start();
+		this.updateMaterialsThread = new Thread(this.updateMaterialsRunnable);
+		this.updateMaterialsThread.start();
 	}
 
 	@Override
 	public void onDisable() {
 		// TODO Insert logic to be performed when the plugin is disabled
 
-		this.updateResourcesRunnable.interrupt();
+		this.updateMaterialsRunnable.interrupt();
 		try {
-			this.updateResourcesThread.join();
+			this.updateMaterialsThread.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ src/main/java/com/wiredlife/mcplugin/concurrent/UpdateMaterialsRunnable.java
@@ -0,0 +1,42 @@
+package com.wiredlife.mcplugin.concurrent;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.wiredlife.jsonformatjava.model.unload.Unload;
+import com.wiredlife.mcplugin.config.Config;
+import com.wiredlife.mcplugin.controller.StorageController;
+
+public class UpdateMaterialsRunnable implements Runnable {
+	
+	@Override
+	public void run() {
+		while (!Thread.currentThread().isInterrupted()) {
+			try {
+				update();
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private synchronized void update() {
+		StorageController storageController = new StorageController(Config.getValues().get("database"));
+
+		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+			List<Unload> unloads = storageController.getUnloads(player.getName());
+			for (Unload unload : unloads) {
+				storageController.updateMaterials(player, unload);
+			}
+		}
+	}
+
+	public synchronized void interrupt() {
+		Thread.currentThread().interrupt();
+	}
+
+}
